00fbeb2ad8b72e20c0a5e0b4222dbad682cb4671
SakshamGupta200 <121724970+SakshamGupta200@users.noreply.github.com>
2024-04-03 15:33:24 +0530

    commit

---
+++ Lab5/Tower_of_hanoi.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void TOH(int n, char source, char aux,  char des) { 
+    if (n > 0) {
+        // Move n-1 disks from source A to target C  
+        // through Target B  
+        TOH(n - 1, source, des, aux);
+        printf("Move disk %d from %c to %c\n", n, source, des); 
+          
+        TOH(n - 1, aux, source, des);  
+    }   
+}
+
+int main(){
+    int n;
+    printf("Enter the number of rings :");
+    scanf("%d",&n);
+    TOH(n,'A','B','C');
+
+
+    return 0;
+}
